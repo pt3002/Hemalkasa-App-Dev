5f6477b418c9914b2865c177ff95f51b85a152b8
bhushansshah <bhushan.shah05@gmail.com>
2023-03-13 17:01:46 +0530

    integrating medicine recyclerview

--- app/src/main/java/com/example/patientregistration/MainActivity.java
+++ app/src/main/java/com/example/patientregistration/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.hemalkasa.hemalkasa.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -28,5 +30,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    
+
 
 }
