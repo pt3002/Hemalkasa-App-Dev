b1d307a644f895cc12687ba6c53570a187763436
ppatil002 <pratikvpatil2002@gmail.com>
2024-02-07 00:16:40 +0530

    Password Protection to Doctor Side

--- app/src/main/java/com/hemalkasa/hemalkasa/Patient_Home_Page.java
+++ app/src/main/java/com/hemalkasa/hemalkasa/Patient_Home_Page.java
@@ -1,8 +1,6 @@
 package com.hemalkasa.hemalkasa;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,25 +8,33 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Patient_Home_Page extends AppCompatActivity {
-    Button Video,Summary,Notification,Risk,Emergency;
+    Button Video, Summary, Notification, Risk, Emergency;
+    // TODO Hardcoded password
+    final static String password="1234";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patient_home_page);
 
-        Video=findViewById(R.id.Video);
-        Summary=findViewById(R.id.Summary);
-        Notification=findViewById(R.id.Notification);
-        Risk=findViewById(R.id.Risk);
-        Emergency=findViewById(R.id.Emergency);
+        Video = findViewById(R.id.Video);
+        Summary = findViewById(R.id.Summary);
+        Notification = findViewById(R.id.Notification);
+        Risk = findViewById(R.id.Risk);
+        Emergency = findViewById(R.id.Emergency);
 
         Video.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Patient_Home_Page.this,Video_MainScreen.class);
+                Intent intent = new Intent(Patient_Home_Page.this, Video_MainScreen.class);
                 startActivity(intent);
             }
         });
@@ -53,7 +59,7 @@ public class Patient_Home_Page extends AppCompatActivity {
         Emergency.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Patient_Home_Page.this,Emergency_Contact.class);
+                Intent intent = new Intent(Patient_Home_Page.this, Emergency_Contact.class);
                 startActivity(intent);
             }
         });
@@ -61,21 +67,54 @@ public class Patient_Home_Page extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(@NonNull Menu menu) {
-        MenuInflater menuInflater=getMenuInflater();
+        MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.patient_to_doctor, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.ChangeToDoctor:
-                Intent intent=new Intent(Patient_Home_Page.this,MainActivity.class);
-                startActivity(intent);
-//                finish();
+                checkpassword();
+//                Intent intent=new Intent(Patient_Home_Page.this,MainActivity.class);
+//                startActivity(intent);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void checkpassword() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Patient_Home_Page.this);
+        builder.setMessage("Permission Needed to Make Call")
+                .setTitle("Enter Password")
+                .setCancelable(true);
+
+        final View PasswordLayout = getLayoutInflater().inflate(R.layout.doctor_password_layout, null);
+        builder.setView(PasswordLayout);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        EditText Password = PasswordLayout.findViewById(R.id.password);
+                        if(Password.getText().toString().equals(password)){
+                            dialog.dismiss();
+                            Toast.makeText(Patient_Home_Page.this, "Successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(Patient_Home_Page.this, MainActivity.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(Patient_Home_Page.this, "Invalid Password", Toast.LENGTH_SHORT).show();
+                            Password.setText("");
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        builder.show();
     }
 }
